b64cce559121192b5342dbd571f3e000751a653e
Riyad <eng.riyadalali93@gmail.com>
2021-04-17 22:49:20 +0300

    Created Splash screen image

--- lib/ui/splashscreen.dart
+++ lib/ui/splashscreen.dart
@@ -11,8 +11,38 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Text("Hello dear Friend bushra"),
+        body: ColumnElement(),
+      ),
+    );
+  } // end build
+
+  //---------------------------------Widget Tree------------------------
+  Widget ColumnElement() {
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+        ),
+        imageBackground(),
+      ],
+    );
+  }
+
+  //---------------------------------------------------
+  Widget imageBackground() {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height * 0.6,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60.0),
+          image: DecorationImage(
+              image: AssetImage('assets/ui/splashscreen/appicon.png'),
+              fit: BoxFit.cover),
+        ),
       ),
     );
   }
-}
+//----------------------------------------------------------------
+} //end class
