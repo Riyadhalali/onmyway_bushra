24fd761e813dca4d5b5f8c864086c9603db4c6e8
Riyad <eng.riyadalali93@gmail.com>
2021-04-21 10:44:11 +0300

    Uploaded the whole app

---
+++ lib/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  // this class to handle all strings for my app
+  static var api_link =
+      'http://jawhara-001-site1.ctempurl.com/Controllers/api.asmx/';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,90 @@
+import 'package:alatareekeh/ui/forgetpassword.dart';
+import 'package:alatareekeh/ui/home.dart';
+import 'package:alatareekeh/ui/languageselect.dart';
+import 'package:alatareekeh/ui/myappointments.dart';
+import 'package:alatareekeh/ui/myservices.dart';
+import 'package:alatareekeh/ui/navigationbar.dart';
+import 'package:alatareekeh/ui/privacypolicy.dart';
+import 'package:alatareekeh/ui/register.dart';
+import 'package:alatareekeh/ui/seekedServices.dart';
+import 'package:alatareekeh/ui/signin.dart';
+import 'package:alatareekeh/ui/splash.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:onmyway_bushra/ui/languageselect.dart';
-import 'package:onmyway_bushra/ui/splashscreen.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:sizer/sizer.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/resources/strings', // <-- change patch to your
+      fallbackLocale: Locale('en', 'US'),
+      child: MyApp()));
+  configLoading();
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..displayDuration = const Duration(milliseconds: 2000)
+    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+    ..loadingStyle = EasyLoadingStyle.light
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.yellow
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = false;
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: SplashScreen.id,
-      routes: {
-        SplashScreen.id: (context) =>
-            SplashScreen(), //create a referenece for this screen
-        LanguageSelect.id: (context) => LanguageSelect(),
+    return LayoutBuilder(
+      //return LayoutBuilder
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          //return OrientationBuilder
+          builder: (context, orientation) {
+            // var lang = context.locale.toString(); // get lang of app
+            // print('your app language is:$lang');
+            //initialize SizerUtil()
+            SizerUtil().init(constraints, orientation); //initialize SizerUtil
+            return MaterialApp(
+              builder: EasyLoading.init(), // init the easy loading
+              debugShowCheckedModeBanner: false,
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+              initialRoute:
+                  SplashScreen.id, // the default screen that will start
+              theme: ThemeData.light(),
+              routes: {
+                //-> we must add all routes
+                SplashScreen.id: (context) => SplashScreen(),
+                LanguageSelect.id: (context) => LanguageSelect(),
+                PrivacyPolicy.id: (context) =>
+                    PrivacyPolicy(), // privacy policy route screen
+                HomePage.id: (context) => HomePage(),
+                SignIn.id: (context) => SignIn(),
+                Register.id: (context) => Register(),
+                ForgetPassword.id: (context) => ForgetPassword(),
+                Navigation.id: (context) => Navigation(),
+
+                MyAppointment.id: (context) => MyAppointment(),
+                MyServices.id: (context) => MyServices(),
+
+                SeekedServices.id: (context) => SeekedServices(),
+              },
+            );
+          },
+        );
       },
     );
   }
